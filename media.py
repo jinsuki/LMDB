e902b1da82e64d8e1ea305c41ab6f4c7a7572de4
Wesley Liang <xjinsuki@gmail.com>
2016-11-06 21:30:43 -0800

    add more objects
    added more movies and shows

--- media.py
+++ media.py
@@ -1,23 +1,21 @@
 class Video():
-    def __init__(self, duration, image, storyline, title):
+    def __init__(self, duration, image, storyline,
+                    title, link):
         self.duration = duration
         self.image = image
         self.storyline = storyline
         self.title = title
-        
-class Movie(Video):
-    def __init__(self, duration, image, storyline, title, link):
-        Video.__init__(self, duration, image, storyline, title)
         self.link = link
+        def view_trailer():
 
-    def view_trailer():
-        
-        
-    
+class Movie(Video):
+    def __init__(self, duration, image, storyline,
+                    title, link):
+        Video.__init__(self, duration, image, storyline, title, link)
 
 class TVShow(Video):
-    def __init__(self, duration, image, storyline, title, episode, season):
-        Video.__init__(self, duration, image, storyline, title)
+    def __init__(self, duration, image, storyline,
+                    title, link, episode, season):
+        Video.__init__(self, duration, image, storyline, title, link)
         self.episode = episode
         self.season = season
-
--- media_list.py
+++ media_list.py
@@ -1,16 +1,45 @@
 import media
 
-breaking_bad = media.TVShow(58,
-                           "http://www.gstatic.com/tv/thumb/tvbanners/9181462/\
-                            p9181462_b_v8_aa.jpg",
-                           "A high school chemistry teacher diagnosed with \
-                           inoperable lung cancer turns to manufacturing   \
-                           and selling methamphetamine in order to secure  \
-                           his family's future.",
-                           "Breaking Bad",
-                           1,
-                           1)
-                           
-                           
-                           
-)
+#instantiate tv_show objects
+breaking_bad = media.TVShow(
+    58,
+    "http://www.gstatic.com/tv/thumb/tvbanners/9181462/p9181462_b_v8_aa.jpg",
+    "A high school chemistry teacher diagnosed with \
+    inoperable lung cancer turns to manufacturing   \
+    and selling methamphetamine in order to secure  \
+    his family's future.",
+    "Breaking Bad",
+    "https://www.youtube.com/watch?v=HhesaQXLuRY",
+    1,
+    1)
+
+how_i_met_your_mother = media.TVShow(
+    30,
+    "http://i2.listal.com/image/205965/600full-how-i-met-your-mother-poster.jpg",
+    "A father recounts to his children, through a \
+    series of flashbacks, the journey he and his  \
+    four best friends took leading up to him      \
+    meeting their mother.",
+    "How I Met Your Mother",
+    "https://www.youtube.com/watch?v=yOe4_kdqsmU",
+    1,
+    1)
+
+#instantiate movie objects
+shutter_island = media.Movie(
+    138,
+    "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg",
+    "a U.S. marshal investigates the disappearance of \
+    a murderess who escaped from a hospital for the   \
+    criminally insane.",
+    "Shutter Island",
+    "https://www.youtube.com/watch?v=5iaYLCiq5RM")
+
+man_who_knew_infinity = media.Movie(
+    140,
+    "http://www.gridcitymagazine.com/wp-content/uploads/2016/09/The-Man-Who-Knew-Infinity-.jpg",
+    " The story of the life and academic career of \
+      the pioneer Indian mathematician,            \
+      Srinivasa Ramanujan",
+    "The Man Who Knew Infinity",
+    "https://www.youtube.com/watch?v=oXGm9Vlfx4w")
